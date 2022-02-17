86d22c12c0832e2db05aa8686ea16d342c3e8f6e
Tianshu Huang <thetianshuhuang@gmail.com>
2022-02-16 22:30:33 -0500

    Version bump should be 2.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='printtools',
-    version='1.3',
+    version='2.0',
     packages=['printtools'],
     license='MIT',
     install_requires='pyfiglet',
